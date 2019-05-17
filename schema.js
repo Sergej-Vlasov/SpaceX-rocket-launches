9454ff49bbc388170b0e2738f1b1429b81b2d643
Sergej-Vlasov <sergej.s.vlasov@gmail.com>
2019-05-17 22:53:45 +0100

    add rockets and rocket queries to RootQuery

--- schema.js
+++ schema.js
@@ -56,6 +56,25 @@ const RootQuery = new GraphQLObjectType({
                     .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
                     .then(res => res.data);
             }
+        },
+        rockets: {
+            type: new GraphQLList(RocketType),
+            resolve(parrent, args) {
+                return axios
+                    .get('https://api.spacexdata.com/v3/rockets')
+                    .then(res => res.data);
+            }
+        },
+        rocket: {
+            type: RocketType,
+            args: {
+                rocket_id: { type: GraphQLString }
+            },
+            resolve(parent, args) {
+                return axios
+                    .get(`https://api.spacexdata.com/v3/rockets/${args.rocket_id}`)
+                    .then(res => res.data);
+            }
         }
     }
 });
