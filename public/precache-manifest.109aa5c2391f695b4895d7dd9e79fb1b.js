189a7cedbb58d9b9968a00ca978f276e83e962aa
Sergej-Vlasov <sergej.s.vlasov@gmail.com>
2019-05-18 12:44:52 +0100

    created build version in /public, ready to be deployed

---
+++ public/precache-manifest.109aa5c2391f695b4895d7dd9e79fb1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077d0b9049d76fd5f2a15c13aec494fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4959f5540b877ef0edf2",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "32ca6a7cfeac65dcc5d1",
+    "url": "/static/js/2.a6765a83.chunk.js"
+  },
+  {
+    "revision": "4959f5540b877ef0edf2",
+    "url": "/static/js/main.0cc282ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
